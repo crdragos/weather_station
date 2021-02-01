294e204d2e242bc8817f51bffdd8bf9390ac27ae
Crecan Dragos - Alexandru <dcrecan@icloud.com>
2021-02-01 20:57:56 +0200

    create project & update readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# weather_station
+
+This application gives users the ability to look up the weather for any location. 
+
+## User Requirements
+* [ ] Search for a location and see weather forecast
+* [ ] Toggle between the metric and imperial system
+* [ ] Refresh the weather for the selected location
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Weather Station'),
+      ),
+    );
+  }
+}
